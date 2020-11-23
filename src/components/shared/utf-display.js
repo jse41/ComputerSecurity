6626c157bf04593a671ac9764cc5a73ea0b75b1c
Lucas Popp <lmp122@case.edu>
2020-11-23 11:16:42 -0500

    Add UTF Display

---
+++ src/components/shared/utf-display.js
@@ -0,0 +1,38 @@
+import _ from 'lodash';
+import React from 'react';
+import PropTypes from 'prop-types';
+import bitHandling from "../../bit-handling-2";
+import './utf-display.css';
+
+const UTFDisplay = ({ ascii }) => {
+    const letters = ascii.split('');
+    const charCodes = letters.map(s => s.charCodeAt(0));
+    const bitCodes = _.chunk(bitHandling.strToBits(ascii).split(''), 16).map(bits => bits.join(''));
+
+    return (
+        <table className="utf-display">
+            <tr>
+                {letters.map(letter => (
+                    <td>
+                        <h1>{letter}</h1>
+                    </td>
+                ))}
+            </tr>
+            <tr>
+                {bitCodes.map(code => (
+                    <td>
+                        <div style={{ fontFamily: 'monospace' }}>
+                            {code}
+                        </div>
+                    </td>
+                ))}
+            </tr>
+        </table>
+    )
+}
+
+UTFDisplay.propTypes = {
+    ascii: PropTypes.string,
+};
+
+export default UTFDisplay;
