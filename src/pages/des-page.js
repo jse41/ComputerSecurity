fa6255df3ef6d6c7297dae957e59eb0ab27df1a5
Lucas Popp <lmp122@case.edu>
2020-11-23 10:51:18 -0500

    Left-align DES page

--- src/pages/des-page.js
+++ src/pages/des-page.js
@@ -217,79 +217,83 @@ class DesPage extends React.Component {
     render() {
         return (
             <Page title="DES">
-                <Form>
-                    <Form.Group controlId="EncryptUpdate">
-                        <Form.Label>Message</Form.Label>
-                        <Form.Control type="text" value={this.state.plaintext}
-                                      onChange={this.handleUpdate.bind(this, 'plaintext')}
-                                      placeholder="Message to Encrypt"/>
-                        <Form.Text className="text-muted">
-                            This is the plain text information you want to share.
-                        </Form.Text>
-                    </Form.Group>
-                    <Form.Group controlId="KeyControl">
-                        <Form.Label>Key</Form.Label>
-                        <Form.Control type="text" value={this.state.key} onChange={this.handleUpdate.bind(this, 'key')}
-                                      placeholder="Key to Use"/>
-                        <Form.Text className="text-muted">
-                            The key to use (any string of 4 characters)
-                        </Form.Text>
-                    </Form.Group>
-                    <Button variant="primary" onClick={this.doEncryption}>
-                        Encrypt
-                    </Button>
-                </Form>
-                <br></br>
-                <h3>Key Generation</h3>
-                <p>Key 1: {this.state.keys[0]}</p>
-                <p>Key 2: {this.state.keys[1]}</p>
-                <br></br>
-                <h3>Encryption</h3>
-                <p>Original Message: {this.state.plaintext}</p>
-                <p>First 64 Bits of Message: {this.state.first64BitBlock}</p>
-                <p>Bits of First Block: {this.state.first64Bits}</p>
-                <br></br>
-                <h5>Initial Permutation</h5>
-                <p>Permutation Table: {this.state.IP}</p>
-                <p>Permutation is the act of mapping with input bit to a new output position. In this permutation, the
-                    input is 64 bits and the output is 64 bits, no bits
-                    are lost or created, instead each and every bit is mapped to a single new location. For example,
-                    since the first entry in the permutation table is
-                    {this.state.IP[0]}, the bit at that index in the input becomes the first bit of the output.</p>
-                <p>After Permutating: {this.state.afterInitialPermutation}</p>
-                <h5>DES Rounds</h5>
-                <p>L0: {this.state.L0}</p>
-                <p>R0: {this.state.R0}</p>
-                <p>Insert latex equation</p>
-                <Collapsible trigger="f Function">
-                    <p>Overview of f function</p>
-                    <p>Expansion Box: {this.state.expansionBox}</p>
-                    <p>After Expansion: {this.state.afterExpansionBox}</p>
-                    <p>XOR with the key:</p>
-                    <p>Key: {this.state.keys[0]}</p>
-                    <p>After XOR: {this.state.afterXorWithKey}</p>
-                    <Collapsible trigger="S Boxes">
-                        <p>First 6 bits of input: {this.state.afterXorWithKey.substring(0, 6)}</p>
-                        <p>Row (formed with the first and last bit of the
-                            input): {this.state.afterXorWithKey.charAt(0) + this.state.afterXorWithKey.charAt(5)}</p>
-                        <p>Column (formed with the middle 4 bits of the
-                            input): {this.state.afterXorWithKey.substring(1, 5)}</p>
-                        <p>Using the row and column calculated above, the corresponding table entry at that location is
-                            the new output.</p>
+                <div style={{ textAlign: 'left' }}>
+                    <Form>
+                        <Form.Group controlId="EncryptUpdate">
+                            <Form.Label>Message</Form.Label>
+                            <Form.Control type="text" value={this.state.plaintext}
+                                          onChange={this.handleUpdate.bind(this, 'plaintext')}
+                                          placeholder="Message to Encrypt"/>
+                            <Form.Text className="text-muted">
+                                This is the plain text information you want to share.
+                            </Form.Text>
+                        </Form.Group>
+                        <Form.Group controlId="KeyControl">
+                            <Form.Label>Key</Form.Label>
+                            <Form.Control type="text" value={this.state.key} onChange={this.handleUpdate.bind(this, 'key')}
+                                          placeholder="Key to Use"/>
+                            <Form.Text className="text-muted">
+                                The key to use (any string of 4 characters)
+                            </Form.Text>
+                        </Form.Group>
+                        <div style={{ textAlign: 'center' }}>
+                            <Button variant="primary" onClick={this.doEncryption}>
+                                Encrypt
+                            </Button>
+                        </div>
+                    </Form>
+                    <br></br>
+                    <h3>Key Generation</h3>
+                    <p>Key 1: {this.state.keys[0]}</p>
+                    <p>Key 2: {this.state.keys[1]}</p>
+                    <br></br>
+                    <h3>Encryption</h3>
+                    <p>Original Message: {this.state.plaintext}</p>
+                    <p>First 64 Bits of Message: {this.state.first64BitBlock}</p>
+                    <p>Bits of First Block: {this.state.first64Bits}</p>
+                    <br></br>
+                    <h5>Initial Permutation</h5>
+                    <p>Permutation Table: {this.state.IP}</p>
+                    <p>Permutation is the act of mapping with input bit to a new output position. In this permutation, the
+                        input is 64 bits and the output is 64 bits, no bits
+                        are lost or created, instead each and every bit is mapped to a single new location. For example,
+                        since the first entry in the permutation table is
+                        {this.state.IP[0]}, the bit at that index in the input becomes the first bit of the output.</p>
+                    <p>After Permutating: {this.state.afterInitialPermutation}</p>
+                    <h5>DES Rounds</h5>
+                    <p>L0: {this.state.L0}</p>
+                    <p>R0: {this.state.R0}</p>
+                    <p>Insert latex equation</p>
+                    <Collapsible trigger="f Function">
+                        <p>Overview of f function</p>
+                        <p>Expansion Box: {this.state.expansionBox}</p>
+                        <p>After Expansion: {this.state.afterExpansionBox}</p>
+                        <p>XOR with the key:</p>
+                        <p>Key: {this.state.keys[0]}</p>
+                        <p>After XOR: {this.state.afterXorWithKey}</p>
+                        <Collapsible trigger="S Boxes">
+                            <p>First 6 bits of input: {this.state.afterXorWithKey.substring(0, 6)}</p>
+                            <p>Row (formed with the first and last bit of the
+                                input): {this.state.afterXorWithKey.charAt(0) + this.state.afterXorWithKey.charAt(5)}</p>
+                            <p>Column (formed with the middle 4 bits of the
+                                input): {this.state.afterXorWithKey.substring(1, 5)}</p>
+                            <p>Using the row and column calculated above, the corresponding table entry at that location is
+                                the new output.</p>
+                        </Collapsible>
+                        <p>After Permutation: {this.state.afterPermutation}</p>
                     </Collapsible>
-                    <p>After Permutation: {this.state.afterPermutation}</p>
-                </Collapsible>
-                <br></br>
-                <hr></hr>
-                <br></br>
-                <p>Your encrypted message:</p>
-                <p>{this.state.encryptedPlaintext}</p>
-                <br></br>
-                <p>The decrypted message:</p>
-                <p>{this.state.decryptedCiphertext}</p>
-                <br></br>
-                <br></br>
-                <br></br>
+                    <br></br>
+                    <hr></hr>
+                    <br></br>
+                    <p>Your encrypted message:</p>
+                    <p>{this.state.encryptedPlaintext}</p>
+                    <br></br>
+                    <p>The decrypted message:</p>
+                    <p>{this.state.decryptedCiphertext}</p>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                </div>
             </Page>
         )
     }
